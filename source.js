938046ff8568450871a170291420d63a629df568
Moha0170 <mohalaja06@gmail.com>
2023-12-19 16:49:19 +0100

    Elkeszult a legkisebb szam kiiratasa, ezen kivul ket <hr>-rel elvalasztottam egymastol a dolgokat hogy atlathatobb legyen.

--- source.js
+++ source.js
@@ -1,13 +1,19 @@
 lista = []
 
 document.getElementById("hozzaad").addEventListener("click", () => {
-    lista.push(document.getElementById("mezo").value);
+    lista.push(parseInt(document.getElementById("mezo").value));
     console.log(lista);
     listaDiv = document.getElementById("lista"); 
     listaDiv.innerHTML = "";
+    b = parseInt(lista[0])
+    document.getElementById("legkisebb").innerText = "A legkisebb szám a listából: " + b
     for (let i = 0; i < lista.length; i++) {
         a = document.createElement("p");
         a.innerHTML = lista[i]
         listaDiv.appendChild(a);
+        if (parseInt(lista[i]) < b) {
+            document.getElementById("legkisebb").innerText = "A legkisebb szám a listából: " + lista[i]
+            b = parseInt(lista[i])
+        }
     }
 });
