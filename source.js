9203a81ca518c16430b7c4f7a75e7bedf9f60b2a
Moha0170 <mohalaja06@gmail.com>
2023-12-19 16:19:17 +0100

    A listából a számokat mostmár kiíratom egy <div>-be, illetve a html-ben megváltoztattam a lista div class-t id-re

--- source.js
+++ source.js
@@ -3,4 +3,11 @@ lista = []
 document.getElementById("hozzaad").addEventListener("click", () => {
     lista.push(document.getElementById("mezo").value);
     console.log(lista);
+    listaDiv = document.getElementById("lista"); 
+    listaDiv.innerHTML = "";
+    for (let i = 0; i < lista.length; i++) {
+        a = document.createElement("p");
+        a.innerHTML = lista[i]
+        listaDiv.appendChild(a);
+    }
 });
